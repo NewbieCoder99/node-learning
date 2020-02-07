e7da8109112708a30cfc799cf5e5c3fb81062fe0
NewbieCoder99 <fajarpunya99@gmail.com>
2020-02-08 01:21:47 +0700

    Latest update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# node-learning
+# Run With Nodemon
+	nodemon start
+# Running Migrate
+	npx sequelize-cli db:migrate
+# Running Seeder
+	npx sequelize-cli db:seed:all
+# Generate Migration
+	npx sequelize-cli model:generate --name Table --attributes field1:string,field2:string
--- seeders/20200204085705-UsersTableSeeder.js
+++ seeders/20200204085705-UsersTableSeeder.js
@@ -2,17 +2,16 @@
 
 const   crypto  = require('crypto'),
         dotenv  = require('dotenv').config(),
-        secret  = process.env.PASSWORD_SECRET;
+        appKey  = process.env.NODE_APP_KEY;
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
       username : 'admin',
-      password : crypto.createHmac('sha256', secret).update('password').digest('hex'),
+      password : crypto.createHmac('sha256', appKey).update('password').digest('hex'),
       email : 'admin@admin.com'
     }], {});
   },
-
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Users', null, {});
   }
