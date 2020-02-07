d66839cbda02f31e9548b685c19429b9d4d37c64
NewbieCoder99 <fajarpunya99@gmail.com>
2020-02-08 01:42:45 +0700

    Make login page

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
